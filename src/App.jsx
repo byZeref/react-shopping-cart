7eb1269ed3f65f6a5f5e36abe1699e2e3cdfbd2b
Sandor SC <sandorsc941018@gmail.com>
2025-03-20 18:01:58 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  console.log(import.meta.env.VITE_API_URL)
+  return (
+    <main className="main-container">
+      <h1 className="mt-4">Shopping Cart</h1>
+    </main>
+  )
+}
+
+export default App
