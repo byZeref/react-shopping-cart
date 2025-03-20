406ab20acada664f47cb480d5d4d27bb732414b9
Sandor SC <sandorsc941018@gmail.com>
2025-03-20 18:32:08 -0400

    loading products

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import api from './api/axios.js'
+import { useEffect, useState } from "react";
 
 function App() {
-  console.log(import.meta.env.VITE_API_URL)
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    const { data, status } = await api.get('/products')
+      .catch(e => {
+        console.error('error getting products', e)
+      })
+
+    console.log('data', data, status)
+    if (status === 200) {
+      setProducts(data)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, []);
+
   return (
     <main className="main-container">
       <h1 className="mt-4">Shopping Cart</h1>
+      <div>
+        <ul>
+          {
+            products.length && products.map(prod =>
+              <li key={prod.id}>{prod.title}</li>
+            )
+          }
+        </ul>
+      </div>
     </main>
   )
 }
