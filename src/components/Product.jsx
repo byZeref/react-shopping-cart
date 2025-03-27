ad898be098e155c8a79c2491c867ebcc975dff6c
Sandor SC <sandorsc941018@gmail.com>
2025-03-27 17:11:47 -0400

    button to add product to cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,27 @@
+import { CartIcon } from "@/components/icons/Cart.jsx";
+
 export function Product({ product }) {
 
+  const handleClick = () => {
+    console.log('add prod', product)
+  }
+
   return (
-    <>
-      <li className="product-card h-[150px] bg-[#383a3f]">
-        <div className="w-full h-full flex">
-          <img className="h-full w-[120px] min-w-[120px]" src={product.image} alt={product.title}/>
-          <div className="flex flex-col py-2 px-4 w-full gap-1">
-            <h4 className="font-semibold">{product.title}</h4>
-            <p className="text-sm capitalize text-slate-300">{product.category}</p>
-            <p className="font-medium text-xl mt-auto">$ {product.price.toFixed(2)}</p>
-          </div>
+    <li className="product-card h-[150px] bg-[#383a3f]">
+      <div className="w-full h-full flex">
+        <img className="h-full w-[120px] min-w-[120px]" src={product.image} alt={product.title}/>
+        <div className="flex flex-col py-2 px-4 w-full gap-1 relative">
+          <h4 className="font-semibold line-clamp-2">{product.title}</h4>
+          <p className="text-sm capitalize text-slate-300">{product.category}</p>
+          <p className="font-medium text-xl mt-auto">$ {product.price.toFixed(2)}</p>
+          <button
+            className="absolute bottom-2 right-2 border rounded-full p-[6px] hover:bg-emerald-900 cursor-pointer duration-200"
+            onClick={handleClick}
+          >
+            <CartIcon />
+          </button>
         </div>
-      </li>
-    </>
+      </div>
+    </li>
   )
 }
