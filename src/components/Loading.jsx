df3c44b17923d86adef641d40793e9016369aa88
Sandor SC <sandorsc941018@gmail.com>
2025-04-02 14:16:01 -0400

    cart btn shadow and loading bg

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import {LoadingIcon} from "@/components/icons/LoadingIcon.jsx";
 
 export function Loading() {
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center text-white bg-black/50">
       <LoadingIcon width="30" />
     </div>
   )
